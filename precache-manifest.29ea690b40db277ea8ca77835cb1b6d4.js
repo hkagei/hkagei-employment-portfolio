a895f1760272922bdcf4d41059cd2ebe7b28cec8
hkagei <k_dawg_1022@hotmail.com>
2023-01-25 15:49:07 -0700

    Updates

---
+++ precache-manifest.29ea690b40db277ea8ca77835cb1b6d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a169e2f315fca7210d1",
+    "url": "/hkagei-employment-portfolio/static/css/main.784b4c0b.chunk.css"
+  },
+  {
+    "revision": "2a169e2f315fca7210d1",
+    "url": "/hkagei-employment-portfolio/static/js/main.2a169e2f.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/hkagei-employment-portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "c06f9b1a0b67a6e7f2fa",
+    "url": "/hkagei-employment-portfolio/static/js/runtime~main.c06f9b1a.js"
+  },
+  {
+    "revision": "3e37bc79bbd2e543dff123a170cca25a",
+    "url": "/hkagei-employment-portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/employment-portfolio/precache-manifest.304691e93b79d5de80fe898187963735.js"
+  "/hkagei-employment-portfolio/precache-manifest.29ea690b40db277ea8ca77835cb1b6d4.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/employment-portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/hkagei-employment-portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
