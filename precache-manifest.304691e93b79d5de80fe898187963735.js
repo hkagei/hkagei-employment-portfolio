c2eafc5d485945b1fa857673eb810d0044b3d525
hkagei <k_dawg_1022@hotmail.com>
2023-01-25 15:28:59 -0700

    Updates

---
+++ precache-manifest.304691e93b79d5de80fe898187963735.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa3c8ae4b5d0b377a0d5",
+    "url": "/employment-portfolio/static/css/main.784b4c0b.chunk.css"
+  },
+  {
+    "revision": "fa3c8ae4b5d0b377a0d5",
+    "url": "/employment-portfolio/static/js/main.fa3c8ae4.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/employment-portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "e00b7489e50111898d57",
+    "url": "/employment-portfolio/static/js/runtime~main.e00b7489.js"
+  },
+  {
+    "revision": "48287a858c914d94385a007b7a2adbe0",
+    "url": "/employment-portfolio/index.html"
+  }
+];
